05774e4e0262901028199c374cd6f6d139c86ecd
LunaLiu4 <luna.liu829@gmail.com>
2023-10-19 10:33:01 -0500

    GA3

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,27 +1,1 @@
-package org.launchcode.techjobsmvc.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
-
-
-/**
- * Created by LaunchCode
- */
-@Controller
-@RequestMapping("search")
-public class SearchController {
-
-    @GetMapping(value = "")
-    public String search(Model model) {
-        model.addAttribute("columns", columnChoices);
-        return "search";
-    }
-
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
-
-}
-
+package org.launchcode.techjobsmvc.controllers;import org.launchcode.techjobsmvc.models.Job;import org.launchcode.techjobsmvc.models.JobData;import org.springframework.stereotype.Controller;import org.springframework.ui.Model;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PostMapping;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RequestParam;import java.util.ArrayList;import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;/** * Created by LaunchCode */@Controller@RequestMapping("search")public class SearchController {    @GetMapping(value = "")    public String search(Model model) {        model.addAttribute("columns", columnChoices);        return "search";    }    // TODO #3 - Create a handler to process a search request and render the updated search view.    @PostMapping(value = "results")    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){        model.addAttribute("columns", columnChoices);        ArrayList<Job> jobs;        if (searchTerm.equals("all") || searchTerm.equals("")){            jobs = JobData.findAll();        }else {            jobs = JobData.findByColumnAndValue(searchType, searchTerm);        }        model.addAttribute("jobs",jobs);        return "search";    }}
